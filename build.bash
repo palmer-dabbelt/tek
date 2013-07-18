8b9af313880d130dca41c81448bc424dad301fce
Palmer Dabbelt <palmer@dabbelt.com>
2013-07-18 15:23:12 -0700

    Make it harder to use an internal talloc
    This provides a strong warning message to users who try and use their
    own internal talloc library, as it's probably not a good idea.
    Package managers are a Good Thing :).

--- build.bash
+++ build.bash
@@ -14,6 +14,23 @@ then
     CFLAGS="$CFLAGS $(pkg-config talloc --libs)"
     CFLAGS="$CFLAGS -DHAVE_TALLOC"
 else
+    if [[ "$1" != "--allow-internal-talloc" ]]
+    then
+        cat <<EOF
+WARNING: Using internal talloc library
+
+This tool requires the 'talloc' library
+   For Debian-based distros (e.g. Ubuntu):
+      apt-get install libtalloc-dev
+   For RPM-based distros (e.g. Fedora):
+      yum install libtalloc-devel
+   For OS X:
+      port install talloc
+
+Re-call this script with --allow-internal-talloc to enable an internal
+talloc library to be used.  This is NOT recommended.
+EOF
+
     CFLAGS="$CFLAGS -Isrc/extern/"
     CFLAGS="$CFLAGS -DNO_CONFIG_H"
     extras="$SRCDIR/extern/extern/talloc.c"
